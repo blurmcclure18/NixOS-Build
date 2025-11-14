738d7f3cac04a2e68a0a4ec286d85928c34da850
Alec McClure <blurmcclure16@gmail.com>
2025-11-14 10:33:23 -0600

    cleaned up configuration file

--- configuration.nix
+++ configuration.nix
@@ -46,7 +46,7 @@
    networking.hostName = "ILCP-LOC-CHK00"; # Define your hostname.
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+   networking.networkmanager.enable = true;  
 
   # Set your time zone.
    time.timeZone = "America/Chicago";
@@ -54,14 +54,6 @@
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  # };
 
   # Enable the X11 windowing system.
    services.xserver.enable = true;
@@ -80,8 +72,6 @@
    services.displayManager.defaultSession = "plasmax11";
    services.displayManager.autoLogin.enable = true;
    services.displayManager.autoLogin.user = "ilcp_user";
-
-
 
   # Enable CUPS to print documents.
    services.printing.enable = true;
@@ -111,9 +101,6 @@
      description = "Guest User";
      hashedPassword = null;
      extraGroups = [ "networkmanager" ];
-     #packages = with pkgs; [
-     #  tree
-     #];
    };
 
    users.users.ilcp_admin = {
@@ -121,65 +108,28 @@
 	extraGroups = [ "wheel" ];
 	};
 
-  # programs.firefox.enable = true;
-
    nixpkgs.config.allowUnfree = true;
 
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
    environment.systemPackages = with pkgs; [
-     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-     wget
-     git
-     curl
-     neovim
-     firefox
      alacritty
-     vmware-horizon-client
+     curl
+     firefox
+     git
+     neovim
      openconnect
+     vim 
+     vmware-horizon-client
+     wget
    ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
 
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
    services.openssh.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  # NixOS Version
   system.stateVersion = "25.05"; # Did you read the comment?
 
 }
